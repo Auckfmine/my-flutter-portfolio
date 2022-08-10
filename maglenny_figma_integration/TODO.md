06fca9ce6fa33fda057d9d066ac0412878246666
Auckfmine <mouhamedaminerouatbi@gmail.com>
2022-08-10 04:42:17 +0100

    removed build artifacts from the projects

--- maglenny_figma_integration/TODO.md
+++
@@ -1,3 +0,0 @@
-## todo 
- - create reusable widget with textinput and icon : DONE
- - finish implementing register an login screens
--- maglenny_figma_integration/android/app/src/main/kotlin/com/example/maglenny_figma_integration/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.maglenny_figma_integration
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
